df34362a16b18192bc019395b581c003723d41dc
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-04-19 20:34:03 +0300

    fix: update create tag name functional

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-
 import PropTypes from "prop-types";
-const Tag = ({ indexForClassName, tagTitle }) => {
+
+const Tag = ({ tagText }) => {
   return <>
-    <div className={`tag-${indexForClassName}`}>{tagTitle}</div>
+    <div className="tag">{tagText}</div>
     </>;
 };
 Tag.propTypes = {
-  indexForClassName: PropTypes.string,
-  tagTitle: PropTypes.string,
+  tagText: PropTypes.string,
 
 };
 
